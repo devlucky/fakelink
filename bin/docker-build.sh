1818cecbe730d506afc5b25402ef7e09ff94241b
Yohan Robert <yohan.robert@outlook.fr>
2016-10-29 23:52:15 +0200

    Use travis go version to build and run

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+export DOCKER_GO_VERSION=${DOCKER_GO_VERSION:-"1.7"}
 export DOCKER_REGISTRY=${DOCKER_REGISTRY:-""}
 export DOCKER_IMAGE=${DOCKER_IMAGE:-"devlucky/fakelink"}
 export DOCKER_ENVIRONMENT=${DOCKER_ENVIRONMENT:-""}
